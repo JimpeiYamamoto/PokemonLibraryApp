127862b42800cc923c35711d0edc3916beb47453
JimpeiYamamoto <zinpei104@gmail.com>
2024-09-28 14:24:46 +0900

    Initial Commit

---
+++ PokemonLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonLibrary
+//
+//  Created by 山本 迅平 on 9/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
